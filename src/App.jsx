f067d873606de85ec7b3f3e601e3e24524867a79
YogeshwaranYOG <yogeshwaran.selvan@openturf.in>
2023-10-11 20:43:34 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { Cards } from "./components/Cards";
+import data from "./data";
+
+function App() {
+  const cards = data.map((item) => {
+    return <Cards key={item.id} item={item} />;
+  });
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">{cards}</section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Cards = (props) => {
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`../images/${props.item.coverImg}`} className="card--image" />
+      <div className="card--stats">
+        <img src="../images/Star 1.png" className="card--star" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray"> ({props.item.stats.reviewCount}) .</span>
+        <span className="gray">{props.item.location}</span>
+      </div>
+      <p>{props.item.title}</p>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
+};
